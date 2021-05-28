7430a0ad8679036a8b44d81bb8ca2dab7a4d9b6c
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-05-28 21:24:47 +0800

    Change return type to list of dictionaries

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ def predict():
     prediction = model.predict_proba([req])[0]
 
     # Merge 2 arrays into dictionary
-    probability = {key: value for key, value in zip(classes, prediction)}
-    sorted_probability = {}
-    for key in sorted(probability, key=probability.get, reverse=True):
-        sorted_probability[key] = probability[key]
+    probability = [{
+        "label": label,
+        "value": value,
+    } for label, value in zip(classes, prediction)]
+    sorted_probability = sorted(probability, key=lambda l: l["value"], reverse=True)
 
     return jsonify(sorted_probability)
 
