d8971926add4d85738ac365b59689ed6b32ff0d3
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-05-28 21:05:19 +0800

    Added new model and gitignore

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def predict():
     return jsonify(sorted_probability)
 
 if __name__ == "__main__":
-    model = pickle.load(open("models/diseases.pkl", "rb"))
+    model = pickle.load(open("models/model-v2.pkl", "rb"))
     app.run(port=port, debug = True)
